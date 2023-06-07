f9502635cc23805c164409d7014dbb1d302e1c20
hugo098 <hi.rodriguez1998@gmail.com>
2023-06-07 11:23:17 -0400

    Add In-memory db config

---
+++ CLTTechnicalAssessmentApi/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CLTTechnicalAssessmentApi.Models
+{
+    public class User
+    {
+        [Key]
+        public long Document { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Phone { get; set; }
+        [Required]
+        public DateOnly Birthdate { get; set; }
+
+
+    }
+}
--- CLTTechnicalAssessmentApi/Program.cs
+++ CLTTechnicalAssessmentApi/Program.cs
@@ -1,7 +1,13 @@
+using CLTTechnicalAssessmentApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseInMemoryDatabase("UsersDB");
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
